d2bfe93acdef2acc15d68c15c35e709ad635770e
Simon Yu <simony@andrew.cmu.edu>
2025-09-20 22:59:42 +0000

    changing database logic: added isprivate field to topic data model, added isprivate to integer fields, updated OpenAPI schema to include new field

--- src/topics/create.js
+++ src/topics/create.js
@@ -35,6 +35,7 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
+			isPrivate: data.isPrivate ? 1 : 0,
 		};
 
 		if (Array.isArray(data.tags) && data.tags.length) {
