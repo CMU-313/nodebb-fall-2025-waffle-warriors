2e358199f27e21e3f06038a293c5bd99567658a6
Felicia Zhang <f.yufei.zhang@gmail.com>
2025-09-26 23:52:18 +0000

    Update plugin hooking to pass testings

--- nodebb-plugin-anonymous-button/public/js/client.js
+++ nodebb-plugin-anonymous-button/public/js/client.js
@@ -1,7 +1,9 @@
 'use strict';
 
 (function () {
-    require(['components'], (components) => {
+    if (typeof window === 'undefined') return; // Only run in browser
+
+    require(['composer', 'components'], () => {
 
         function toggleAnonButton($composer, $button) {
             let $hidden = $composer.find('input[name="is_anonymous"]');
@@ -18,33 +20,34 @@
             $hidden.val(isAnon ? 'false' : 'true');
 
             $button.toggleClass('active-anonymous', !isAnon);
-
             console.log('[Anonymous Button] toggled, new value =', $hidden.val());
         }
 
-        function setupAnonButtons() {
-            $('.formatting-group [data-format="AnonButton"]').each(function () {
+        function setupAnonButtons($root) {
+            $root.find('.formatting-group [data-format="AnonButton"]').each(function () {
                 const $button = $(this);
                 const $composer = $button.closest('.composer');
 
+                $button.off('click').on('click', () => toggleAnonButton($composer, $button));
+
+                // Initialize button state
                 const $hidden = $composer.find('input[name="is_anonymous"]');
                 if ($hidden.length && $hidden.val() === 'true') {
                     $button.addClass('active-anonymous');
                 } else {
                     $button.removeClass('active-anonymous');
                 }
-
-                $button.off('click').on('click', function () {
-                    toggleAnonButton($composer, $button);
-                });
             });
         }
 
-        // Run when composer is enhanced or after AJAX navigation
-        $(window).on('action:composer.enhanced action:ajaxify.end', setupAnonButtons);
-
+        $(window).on('action:composer.enhanced action:ajaxify.end', () => setupAnonButtons($(document)));
     });
 }());
+
+
+
+
+
 
 
 
