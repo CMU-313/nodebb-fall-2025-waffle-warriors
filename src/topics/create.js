e76052ed5caef727977400bfb4aad6cb836af1dc
Simon Yu <simony@andrew.cmu.edu>
2025-10-10 22:48:20 +0000

    FINALY FIX LINTER

--- src/topics/create.js
+++ src/topics/create.js
@@ -36,7 +36,7 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
-isPrivate: data.isPrivate ? 1 : 0,
+			isPrivate: data.isPrivate ? 1 : 0,
 			is_anonymous: false,
 		};
 		
